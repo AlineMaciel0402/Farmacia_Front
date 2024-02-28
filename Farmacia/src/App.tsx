844f91720c30ff1520a6f1ba9f391c9fe61f1590
AlineMaciel0402 <alinemaciel04@gmail.com>
2024-02-28 12:06:20 -0300

    Projeto com falha

--- Farmacia/src/App.tsx
+++ Farmacia/src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './paginas/home/Home';
+import ListaCategorias from './components/categoria/listaCategoria/ListaCategorias';
+import FormularioCategoria from './components/categoria/formularioCategoria/FormularioCategoria';
+import DeletarCategoria from './components/categoria/deletarCategoria/DeletarCategoria';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
--- Farmacia/src/components/navBar/NavBar.tsx
+++ Farmacia/src/components/navBar/NavBar.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 
 function Navbar() {
- 
+
   
 
   return (
     <>
-     <div className='w-full bg-orange-500 text-white flex justify-center py-4'>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmacia do Povo</div>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
             <div className='flex gap-4'>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Cadastrar Categoria</div>
-              <div className='hover:underline'>Sair</div>
+            <div className='hover:underline'>Postagens</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+              <div className='hover:underline'>Cadastrar tema</div>
             </div>
           </div>
         </div>
